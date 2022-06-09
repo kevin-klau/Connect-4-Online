aa031d7f575797fc380fd239f9b622e480a84410
azayako <62726855+azayako@users.noreply.github.com>
2022-06-09 10:26:04 -0400

    Update LoadingScreen.java
    Some rotations, maybe it will work. But I need to test it in a JFrame first.

--- LoadingScreen.java
+++ LoadingScreen.java
@@ -1,1 +1,34 @@
-public class LoadingScreen
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.io.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import static java.lang.Math.*;
+
+class LoadingScreen extends JPanel{
+
+BufferedImage spinnerPiece;
+int spinnerWidth = spinnerPiece.getWidth();
+int spinnerHeight = spinnerPiece.getHeight();
+
+BufferedImage newImage = new BufferedImage(spinnerWidth, spinnerHeight, BufferedImage.TYPE_INT_ARGB);
+
+public void paintComponent(Graphics g){
+	super.paintComponent(g);
+	g.drawImage(spinnerPiece, 600, 25, this);
+	Graphics2D g2d = (Graphics2D)g;
+	g2d.rotate(Math.toRadians(10), spinnerWidth/2, spinnerHeight/2);
+}
+
+public LoadingScreen(){
+		super();
+		try{
+			spinnerPiece = ImageIO.read(new File("diagram.png"));
+		}catch(IOException e){
+			System.out.println("Unable to load diagram");
+		}
+	}
+	
+}
