0085f5291867561390c173499030d324063035cc
Mucck <106683578+Mucck@users.noreply.github.com>
2022-06-09 10:15:20 -0400

    Update MainPanel.java

--- MainPanel.java
+++ MainPanel.java
@@ -25,9 +25,9 @@ public class MainPanel extends JPanel {
 		g.fillRect (150,50,980,200);
 		
 		g.setColor (Color.RED);
-		TitleFont = new Font("Arial", Font.BOLD, 170);
+		TitleFont = new Font("Arial", Font.BOLD, 180);
 		g.setFont (TitleFont);
-		g.drawString ("Connect 4", 200,200);
+		g.drawString ("Connect 4", 200,210);
 	}
 	
 	// Constructor
