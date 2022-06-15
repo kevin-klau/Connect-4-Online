760579a640feefe84d9ccbd4335e5e9ce1fa1061
azayako <62726855+azayako@users.noreply.github.com>
2022-06-15 17:23:40 -0400

    finished help screen #1
    added correct image sizing and text formatting, adding heading, etc etc

--- HelpScreen1.java
+++ HelpScreen1.java
@@ -21,6 +21,8 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 	/** example board image*/	
 	BufferedImage help3 = null;
 	
+	/** main heading*/
+	JEditorPane helpHeading;
 	/** first text area*/	
 	JEditorPane helpText;
 	/** second text area*/	
@@ -38,8 +40,8 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 		// Images
 		g.drawImage(helpBG, 0,0, null);
 		g.drawImage(help1, 100,500, this);
-		g.drawImage(help2, 300,500, this);
-		g.drawImage(help3, 500,500, this);
+		g.drawImage(help2, 375,499, this);
+		g.drawImage(help3, 652,500, this);
 	}
 	// Constructor	
 	/** constructor, load images, set state of JComponents*/	
@@ -47,24 +49,34 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 		super();
 		try{
 			helpBG = ImageIO.read(new File("HelpBG.png"));
-			help1 = ImageIO.read(new File("SampleBoardHelp1.png"));
-			help2 = ImageIO.read(new File("SampleBoardHelp2.png"));
-			help3 = ImageIO.read(new File("SampleBoardHelp3.png"));
+			help1 = ImageIO.read(new File("SampleHelpBoard1.png"));
+			help2 = ImageIO.read(new File("SampleHelpBoard2.png"));
+			help3 = ImageIO.read(new File("SampleHelpBoard3.png"));
 		}catch(IOException e){
 			System.out.println("Image Error");
 		}
+		helpHeading = new JEditorPane("text/html", "");
+		helpHeading.setText("<h1>Help Page!!!</h1>");
+		helpHeading.setEditable(false);
+		helpHeading.setSize(200,100);
+		helpHeading.setLocation(580,0);
+		helpHeading.setOpaque(false);
+		this.add(helpHeading);
+		
 		helpText = new JEditorPane("text/html", "");
-		helpText.setText("<b>Connecting to the Server: </b> <br>Your IP Address: 123.114.3.25.6\n    -     Make sure you have the 'server' player connect first!\n    -     Make sure you have the 'client' player connect after, with the exact same IP address and Port!");
+		helpText.setText("<h1>Connecting to the Server: </h1> <br>Example IP Address: 123.114.3.25.6<br>    -     Make sure you have the 'server' player connect first!<br>    -     Make sure you have the 'client' player connect after, with the exact same IP address and Port!");
 		helpText.setEditable(false);
-		helpText.setSize(400,200);
+		helpText.setSize(400,150);
 		helpText.setLocation(100, 100);
+		helpText.setOpaque(false);
 		this.add(helpText);
 		
 		helpText2 = new JEditorPane("text/html", "");
-		helpText2.setText("<b>How to play Connect 4: </b> <br>You and your opponents will take turns placing pieces on the board. The first person to get 4 pieces in a row wins (Vertically, Horizontally or Diagonally!\n Here are examples of winning boards (for red):");
+		helpText2.setText("<h1>How to play Connect 4: </h1> <br>You and your opponents will take turns placing pieces on the board. The first person to get 4 pieces in a row wins (Vertically, Horizontally or Diagonally!) Here are examples of winning boards (for red):");
 		helpText2.setEditable(false);
-		helpText2.setSize(400,200);
+		helpText2.setSize(400,150);
 		helpText2.setLocation(100, 300);
+		helpText2.setOpaque(false);
 		this.add(helpText2);
 		
 		continueButton = new JButton();
