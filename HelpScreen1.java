d0ee6f9c5b95e39e68e9cf5e68269f52a51b7aef
azayako <62726855+azayako@users.noreply.github.com>
2022-06-15 16:59:58 -0400

    Update HelpScreen1.java
    added text formatting

--- HelpScreen1.java
+++ HelpScreen1.java
@@ -22,9 +22,9 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 	BufferedImage help3 = null;
 	
 	/** first text area*/	
-	JTextArea helpText;
+	JEditorPane helpText;
 	/** second text area*/	
-	JTextArea helpText2;
+	JEditorPane helpText2;
 	/** continues to next help page*/	
 	JButton continueButton;
 	
@@ -37,9 +37,9 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 		super.paintComponent(g);
 		// Images
 		g.drawImage(helpBG, 0,0, null);
-		g.drawImage(help1, 200,400, this);
-		g.drawImage(help2, 400,400, this);
-		g.drawImage(help3, 600,400, this);
+		g.drawImage(help1, 100,500, this);
+		g.drawImage(help2, 300,500, this);
+		g.drawImage(help3, 500,500, this);
 	}
 	// Constructor	
 	/** constructor, load images, set state of JComponents*/	
@@ -53,18 +53,18 @@ public class HelpScreen1 extends JPanel implements ActionListener{
 		}catch(IOException e){
 			System.out.println("Image Error");
 		}
-		helpText = new JTextArea();
-		helpText.setText("Connecting to the Server: \n Your IP Address: 123.114.3.25.6\n    -     Make sure you have the 'server' player connect first!\n    -     Make sure you have the 'client' player connect after, with the exact same IP address and Port!");
+		helpText = new JEditorPane("text/html", "");
+		helpText.setText("<b>Connecting to the Server: </b> <br>Your IP Address: 123.114.3.25.6\n    -     Make sure you have the 'server' player connect first!\n    -     Make sure you have the 'client' player connect after, with the exact same IP address and Port!");
 		helpText.setEditable(false);
-		helpText.setSize(400,400);
+		helpText.setSize(400,200);
 		helpText.setLocation(100, 100);
 		this.add(helpText);
 		
-		helpText2 = new JTextArea();
-		helpText2.setText("How to play Connect 4: \n You and your opoponents will take turns placing pieces on the board. The first person to get 4 pieces in a row wins (Vertically, Horizontally or Diagonally!\n Here are examples of winning boards (for red):");
+		helpText2 = new JEditorPane("text/html", "");
+		helpText2.setText("<b>How to play Connect 4: </b> <br>You and your opponents will take turns placing pieces on the board. The first person to get 4 pieces in a row wins (Vertically, Horizontally or Diagonally!\n Here are examples of winning boards (for red):");
 		helpText2.setEditable(false);
-		helpText2.setSize(400,400);
-		helpText2.setLocation(500, 100);
+		helpText2.setSize(400,200);
+		helpText2.setLocation(100, 300);
 		this.add(helpText2);
 		
 		continueButton = new JButton();
